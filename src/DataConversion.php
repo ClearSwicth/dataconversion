c90190453d31d4f88c9432d4b3a46f5b5b148cf1
903221813@qq.com <903221813@qq.com>
2023-03-14 12:21:52 +0800

    修改

--- src/DataConversion.php
+++ src/DataConversion.php
@@ -4,13 +4,15 @@
  * User: daikai
  * Date: 2021/4/1
  */
- namespace ClearsWitch\DataConversion;
- use ClearsWitch\DataConversion\DataType\BaseType;
 
- /**
-  * Class DataConversion
-  * @package ClearsWitch\DataConversion
-  */
+namespace ClearsWitch\DataConversion;
+
+use Clearswitch\DoraemonIoc\Container;
+
+/**
+ * Class DataConversion
+ * @package ClearsWitch\DataConversion
+ */
 class DataConversion
 {
     /**
@@ -32,16 +34,28 @@ class DataConversion
     /**
      * @var array
      */
-    private $dataClass=[
-        'json'=>'ClearsWitch\DataConversion\DataType\DataJson',
-        'array'=>'ClearsWitch\DataConversion\DataType\DataArray',
-        'xml'=>'ClearsWitch\DataConversion\DataType\DataXml',
+    private $dataClass = [
+        'json' => 'ClearsWitch\DataConversion\DataType\DataJson',
+        'array' => 'ClearsWitch\DataConversion\DataType\DataArray',
+        'xml' => 'ClearsWitch\DataConversion\DataType\DataXml',
     ];
-
+    /**
+     * 服务容器
+     * @var
+     */
+    private $Container;
     /**
      * @var
      */
     private $useClass;
+
+    public function __construct()
+    {
+        $this->Container = new Container();
+        $this->Container->bind('json', 'ClearsWitch\DataConversion\DataType\DataJson');
+        $this->Container->bind('array', 'ClearsWitch\DataConversion\DataType\DataArray');
+        $this->Container->bind('xml', 'ClearsWitch\DataConversion\DataType\DataXml');
+    }
 
     /**
      * Date: 2021/4/1 3:51 下午
@@ -49,62 +63,40 @@ class DataConversion
      * @param $type
      * @author daikai
      */
-    public function dataConversion($data,$type){
-       $this->data=$data;
-       $this->type=strtolower($type);
-       $this->prepare();
-
-       return $this->conversiontype()->Conversion($this);
+    public function dataConversion($data, $type)
+    {
+        $this->data = $data;
+        $this->type = strtolower($type);
+        $this->prepare();
+        try {
+            return $this->Container->make($this->type)->Conversion($this);
+        } catch (\Exception $re) {
+            return "暂时只支持转换成array,json,xml的格式";
+        }
     }
 
     /**
-     * Date: 2021/4/1 3:22 下午
-     * @return bool
-     * @author daikai
+     * Date: 2023/3/14 下午12:19
+     * @throws \Exception
+     * @author clearSwitch
      */
-    public function prepare(){
-       //print_R(simplexml_load_string($this->data));exit;
-        switch ($this->data){
+    public function prepare()
+    {
+        switch ($this->data) {
             case(is_array($this->data)):
-                $this->requestDataType='array';
+                $this->requestDataType = 'array';
                 break;
             case(!empty(json_decode($this->data))):
-                $this->requestDataType='json';
+                $this->requestDataType = 'json';
                 break;
             case(!empty(@simplexml_load_string($this->data))):
-                $this->requestDataType='xml';
+                $this->requestDataType = 'xml';
                 break;
             default:
-                return false;
+                throw new \Exception("暂时只支持array,json,xml的转换");
                 break;
-        }
-        if(!isset($this->dataClass[$this->type])){
-           echo  '暂只支持数组，xml,json 的转换';exit;
         }
     }
 
-    /**
-     * Date: 2021/4/1 6:21 下午
-     * @return BaseType
-     * @author daikai
-     */
-    public function conversiontype(...$args){
-        return new $this->dataClass[$this->type]([], ...$args);
-    }
-    /**
-     *统一返回的格式
-     * @param array $data
-     * @param string $msg
-     * @param int $code
-     * @return false|string
-     * @author daikai
-     */
-    protected  function black(array $data,$msg='',$code=200){
-        $result=[
-            'code'=>$code,
-            'msg'=>$msg,
-            'data'=>$data
-        ];
-        return json_encode($result,JSON_UNESCAPED_UNICODE);
-    }
+
 }
